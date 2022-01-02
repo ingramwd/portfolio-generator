463630a4ba6f4f487add0cea00247df548d32895
ingramwd <Ingramwd01@gmail.com>
2022-01-02 18:24:00 -0500

    askBCS

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
-
-// const fs = require('fs');
-
-// const generatePage = require('./page-template')
+const generatePage = require('./page-template')
 
 
 const promptUser = () => {
@@ -143,7 +141,13 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
 
 
